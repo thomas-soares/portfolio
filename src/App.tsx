020aab9d7b33878dd0785b54725674c2cdcaf037
Thomas Soares <thomas__soares@hotmail.com>
2025-11-29 21:42:43 -0300

    feat: content of portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,173 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Thomas Soares</h1>
+        <p>Desenvolvedor Front-end na Leega</p>
+        <p>Javascript • Typescript • React • Next.js • Node.js • NestJS</p>
+      </header>
+
+      <main>
+        <section>
+          <h2>Resumo</h2>
+          <p>
+            Como desenvolvedor front-end, trabalho em conjunto com designers e
+            desenvolvedores back-end para criar interfaces de usuário atraentes
+            e funcionais para aplicativos da web. Utilizo tecnologias como HTML,
+            CSS, Javascript, Typescript, React, Next.js, React Native, Graphql,
+            Node.js, Zustand, Redux, Tailwind CSS, Styled Components, Jest,
+            entre outras.
+          </p>
+          <p>
+            Dentro de um time ágil, sigo rotinas Scrum que incluem reuniões
+            diárias, planejamento de sprint, revisão e retrospectiva. Colaboro
+            para garantir que os requisitos do usuário sejam atendidos em cada
+            iteração.
+          </p>
+          <p>
+            Com mais de 11 anos de experiência, desenvolvo interfaces com foco
+            em React e trabalho em equipes multidisciplinares para entregar
+            produtos de forma ágil e eficiente. 🚀
+          </p>
+        </section>
+
+        <section>
+          <h2>Experiência</h2>
+
+          <article>
+            <h3>Leega — Desenvolvedor Front-end</h3>
+            <p>
+              <strong>outubro de 2024 - outubro de 2025</strong>
+            </p>
+            <p>
+              Atuei em um projeto do setor financeiro, migrando uma aplicação de
+              internet banking originalmente em Flutter Web. Também
+              desenvolvemos o Design System baseado no Figma. O sistema foi
+              reescrito com React, Vite, Tailwind CSS, Zustand, Storybook, Jest,
+              TanStack Query, ESLint e Prettier.
+            </p>
+          </article>
+
+          <article>
+            <h3>IBM — Desenvolvedor Front-end</h3>
+            <p>
+              <strong>maio de 2023 - setembro de 2024</strong>
+            </p>
+            <p>
+              Outsourcing para cliente de petróleo e gás, atuando na migração de
+              um sistema legado em ASP para Angular. Utilização de Angular
+              Material, back-end em .NET e Oracle com Dapper e Entity Framework.
+              Paralelamente, estudei mais React e Next.js, minhas principais
+              especialidades.
+            </p>
+          </article>
+
+          <article>
+            <h3>CI&T — Desenvolvedor Front-end</h3>
+            <p>
+              <strong>julho de 2022 - maio de 2023</strong>
+            </p>
+            <p>
+              Outsourcing para cliente do ramo farmacêutico, trabalhando em
+              e-commerce feito em Next.js e em apps das marcas utilizando React
+              Native. Tecnologias: Typescript, React, Next.js, React Native,
+              GraphQL, Node.js, Redux, Styled Components, Jest etc.
+            </p>
+          </article>
+
+          <article>
+            <h3>americanas s.a. — Desenvolvedor Front-end</h3>
+            <p>
+              <strong>abril de 2021 - julho de 2022</strong>
+            </p>
+            <p>
+              Atuação na marca Americanas Empresas, desenvolvendo novas features
+              e modernizando soluções do e-commerce usando Typescript, React,
+              Next.js, Node.js, Redux, Styled Components, Jest, GraphQL.
+            </p>
+          </article>
+
+          <article>
+            <h3>Huia — Desenvolvedor Front-end</h3>
+            <p>
+              <strong>agosto de 2018 - abril de 2021</strong>
+            </p>
+            <p>
+              Desenvolvimento front-end de plugins para DjangoCMS utilizando
+              metodologias ágeis. Tecnologias: Django, DjangoCMS, SASS, REST,
+              Docker, Git, Gitlab, micro serviços. Projetos anteriores com
+              Drupal 8, Wordpress, Laravel, Lumen, EJS, Jenkins, Gulp, Grunt,
+              NPM, Yarn, HTML, CSS, Javascript, Jquery, MySQL, SQLite, PHP,
+              Python.
+            </p>
+          </article>
+
+          <article>
+            <h3>Zeta Informática Ltda. — Desenvolvedor Front-end</h3>
+            <p>
+              <strong>julho de 2017 - agosto de 2018</strong>
+            </p>
+            <p>
+              Atuei na versão web de um sistema legado desenvolvido em NodeJS e
+              AngularJS. Também trabalhei em um site com indicadores para
+              clientes de ERP em Ruby. Participei brevemente do desenvolvimento
+              de emissão de documentos eletrônicos (MDFE e CTE) via integrador
+              Java.
+            </p>
+          </article>
+
+          <article>
+            <h3>Huia — Desenvolvedor Front-end</h3>
+            <p>
+              <strong>março de 2015 - fevereiro de 2017</strong>
+            </p>
+            <p>
+              Tecnologias utilizadas: HTML, CSS, SASS, LESS, Stylus, Bootstrap
+              3, Foundation, BEM, Javascript, Jquery, Gulp, Grunt, Backbone,
+              PHP, Kohana, Wordpress, C#, .NET, SQL Server, MySQL, Jenkins,
+              Composer, NPM, Bower, Git.
+            </p>
+          </article>
+
+          <article>
+            <h3>Ferragem Thony — Assistente de TI</h3>
+            <p>
+              <strong>março de 2014 - março de 2015</strong>
+            </p>
+            <p>
+              Tecnologias utilizadas: HTML, CSS, Javascript, Jquery, MySQL, PHP,
+              OpenCart 1.5.x e migração para Magento.
+            </p>
+          </article>
+        </section>
+
+        <section>
+          <h2>Formação acadêmica</h2>
+
+          <article>
+            <h3>UniRitter</h3>
+            <p>Bacharelado em Ciência da Computação (2015 - 2021)</p>
+          </article>
+
+          <article>
+            <h3>Alcides Maya Tecnologia</h3>
+            <p>Técnico em Informática (2013 - 2014)</p>
+          </article>
+
+          <article>
+            <h3>Micros & Methodos Informática</h3>
+            <p>Técnico em Informática (2011 - 2012)</p>
+          </article>
+        </section>
+      </main>
+
+      <footer>
+        <p>© 2025 — Perfil Profissional de Thomas Soares</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
